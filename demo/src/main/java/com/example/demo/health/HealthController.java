adf9697d240ba42447797a840d2b53a26713e1c6
GabrielSilva2111 <gasilo14@gmail.com>
2024-08-29 22:31:52 -0500

    Initial commito with project

---
+++ demo/src/main/java/com/example/demo/health/HealthController.java
@@ -0,0 +1,13 @@
+package com.example.demo.health;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/health")
+public class HealthController {
+    @GetMapping
+    public String checkAPI() {
+        return "<h1>The API is working ok!</h1>";
+    }
+}
